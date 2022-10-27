7ea8fb0955f2b91137c383807c20e62f15ec41ae
Robertas Misevičius <robertas31810@gmail.com>
2022-10-27 08:51:48 +0300

    forEach

--- js/forEach.js
+++ js/forEach.js
@@ -3,11 +3,18 @@ console.log('forEach.js');
 
 
 
-
+// forEach skirtas labiau side effect'am
 const nums = [1,2,3];
 
-nums.forEach((sk) => {
-    console.log(sk);
+nums.forEach((sk,i,arr) => {
+    console.log(i,sk);
 })
 
 nums.forEach((sk) => console.log('sk ===',sk));
+
+// suskaiciuoti suma
+let suma = 0;
+nums.forEach((sk) => {
+    suma += sk;
+});
+console.log(suma);
